a53b660931acfafebc2694a197229274cba5b9a0
nsherron90 <nsherron90@gmail.com>
2019-04-22 13:42:57 -0400

    publishing

--- spec/filters/greynoise_spec.rb
+++ spec/filters/greynoise_spec.rb
@@ -3,19 +3,33 @@ require_relative '../spec_helper'
 require "logstash/filters/greynoise"
 
 describe LogStash::Filters::Greynoise do
-  describe "Set to Hello World" do
+
+  describe "defaults" do
     let(:config) do <<-CONFIG
       filter {
         greynoise {
-          message => "Hello World"
+          ip => "ip"
         }
       }
     CONFIG
-    end
+    # end
 
-    sample("message" => "some text") do
-      expect(subject).to include("message")
-      expect(subject.get('message')).to eq('Hello World')
+    sample("ip" => "8.8.8.8") do
+      insist { subject }.include?("greynoise")
+
+      expected_fields = %w(greynoise.ip greynoise.seen)
+      expected_fields.each do |f|
+        insist { subject.get("greynoise") }.include?(f)
+      end
+    end
     end
   end
 end
+#
+#
+#     sample("message" => "some text") do
+#       expect(subject).to include("message")
+#       expect(subject.get('message')).to eq('Hello World')
+#     end
+#   end
+# end
